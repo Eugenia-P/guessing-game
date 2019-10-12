8e2b9dbfbb5e8496146e89c6d444e10bd6eae11c
Eugenia-P <eugenia.protasevich@yandex.by>
2019-10-13 01:23:02 +0300

    feat: my solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,36 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+    //Minimum and maximum values are set describing the range 
+    //to search in for the guessing number 
 
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
     }
+
+    //The method is used to guess the number
+    //Binary search is implemented to half the amount of possible answers   
 
     guess() {
 
+        return this.middle = Math.round((this.min + this.max) / 2);
     }
+
+    //The method sets the maximum point of the range 
+    //when the middle is greater than the guessing number
 
     lower() {
-
+        this.max = this.middle;
     }
+
+    //The method sets the minimum point of the range 
+    //when the middle is lower than the guessing number
 
     greater() {
-
+        this.min = this.middle;
     }
 }
+
 
 module.exports = GuessingGame;
